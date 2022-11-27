5d1453e15937df318a7aeb2424eb80736f406be9
Alejandro Amaral <aamaral@logico.com.ar>
2022-11-27 18:14:22 -0300

    Working endpoints

--- server/index.js
+++ server/index.js
@@ -5,27 +5,87 @@ const GRPC_PORT = 5000
 var grpc = require("@grpc/grpc-js");
 var protoLoader = require("@grpc/proto-loader");
 
+const quotes = [
+  {
+    id: 1,
+    quote: "Frankly, my dear, I don't give a damn",
+    author: "Gone With the Wind"
+  },
+  {
+    id: 2,
+    quote: "I'm going to make him an offer he can't refuse",
+    author: "The Godfather"
+  },
+  {
+    id: 3,
+    quote: "Go ahead, make my day",
+    author: "Sudden Impact"
+  },
+  {
+    id: 4,
+    quote: "I love the smell of napalm in the morning",
+    author: "Apocalyse Now"
+  },
+  {
+    id: 5,
+    quote: "You're gonna need a bigger boat",
+    author: "Jaws"
+  },
+]
+
+
+// Add REST routes
+app.get('/api/quotes/', (req, res) => {
+  res.json(quotes)
+})
+
+app.get('/api/quotes/:id', (req, res) => {
+  res.json(getQuoteById(req.params.id))
+})
+
+function getQuoteById(id) {
+  // Return single object quote
+  return quotes.filter(quote => quote.id == id)[0] || {}
+}
+
 let pkgDefinition = protoLoader.loadSync(
   "./src/grpc/protos/quotes.proto",
   {
     keepCase: true,
-    longs: BigInt,
+    longs: String,
     enums: String,
-    arrays: true
+    arrays: true,
+    defaults: true,
+    oneofs: true
   })
+let quotesProto = grpc.loadPackageDefinition(pkgDefinition);
 
 
+const gRpcServer = new grpc.Server();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+gRpcServer.addService(quotesProto.QuoteService.service, {
+  get: (_, callback) => {
+    callback(null, { quotes })
+  },
+
+  getOne: (call, callback) => {
+    let quote = getQuoteById(call.request.id)
+
+    if (quote) {
+      callback(null, quote);
+    } else {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        details: `Quote id ${call.request.id} not found`
+      })
+    }
+  }
+});
 
 
+// Start both REST and gRPC servers
 async function serverStart() {
-  let quotesProto = grpc.loadPackageDefinition(pkgDefinition);
-  const server = new grpc.Server();
-
-  server.bindAsync(
+  gRpcServer.bindAsync(
     `127.0.0.1:${GRPC_PORT}`,
     grpc.ServerCredentials.createInsecure(),
     (err) => {
@@ -34,15 +94,12 @@ async function serverStart() {
         return null;
       }
 
-
-      server.start();
+      gRpcServer.start();
       console.log(`gRPC listening on ${GRPC_PORT}`)
       app.listen(REST_PORT, () => {
         console.log(`REST listening on ${REST_PORT}`)
       })
     });
-
-
 }
 
 serverStart()
