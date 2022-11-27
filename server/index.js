5c50f2bfa4cd4d13508f8a384a56e7f9c5a8341a
Alejandro Amaral <aamaral@logico.com.ar>
2022-11-27 17:13:50 -0300

    Server definition

--- server/index.js
+++ server/index.js
@@ -1,12 +1,48 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const REST_PORT = 3000
+const GRPC_PORT = 5000
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
+
+let pkgDefinition = protoLoader.loadSync(
+  "./src/grpc/protos/quotes.proto",
+  {
+    keepCase: true,
+    longs: BigInt,
+    enums: String,
+    arrays: true
+  })
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
+async function serverStart() {
+  let quotesProto = grpc.loadPackageDefinition(pkgDefinition);
+  const server = new grpc.Server();
+
+  server.bindAsync(
+    `127.0.0.1:${GRPC_PORT}`,
+    grpc.ServerCredentials.createInsecure(),
+    (err) => {
+      if (err) {
+        console.error(err.message);
+        return null;
+      }
+
+
+      server.start();
+      console.log(`gRPC listening on ${GRPC_PORT}`)
+      app.listen(REST_PORT, () => {
+        console.log(`REST listening on ${REST_PORT}`)
+      })
+    });
+
+
+}
+
+serverStart()
