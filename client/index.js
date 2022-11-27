c3e182a60f9a1cf5f4d761ceaa9b5203deea1400
Alejandro Amaral <aamaral@logico.com.ar>
2022-11-27 19:31:05 -0300

    Working client

--- client/index.js
+++ client/index.js
@@ -1,12 +1,50 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const REST_PORT = 3001
+const GRPC_PORT = 5000
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// gRPC services setup
+let pkgDefinition = protoLoader.loadSync(
+  "./src/grpc/protos/quotes.proto",
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    arrays: true,
+    defaults: true,
+    oneofs: true
+  })
+
+const QuoteService = grpc.loadPackageDefinition(pkgDefinition).QuoteService;
+const client = new QuoteService(
+  `127.0.0.1:${GRPC_PORT}`,
+  grpc.credentials.createInsecure()
+)
+
+
+app.get('/api/grpc/quotes/', (req, res) => {
+  client.get(null, (err, data) => {
+    if (err) {
+      return res.status(err.code).send(err.details)
+    }
+
+    return res.send(data)
+  })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/api/grpc/quotes/:id', (req, res) => {
+  res.json(client.getOne(req.params.id, (err, data) => {
+    if (err) {
+      return res.status(err.code).send(err.details)
+    }
+
+    res.json(data)
+  }))
+})
+
+app.listen(REST_PORT, () => {
+  console.log(`REST listening on ${REST_PORT}`)
 })
 
