1116e8badf094346008210b917c9fc9ee0aa8abe
Alejandro Amaral <aamaral@logico.com.ar>
2022-11-27 21:43:44 -0300

    REST APIs working

--- client/index.js
+++ client/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const REST_PORT = 3001
 const GRPC_PORT = 5000
-var grpc = require("@grpc/grpc-js");
-var protoLoader = require("@grpc/proto-loader");
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const axios = require('axios').default;
+const SERVER_URL = 'http://localhost:3000'
 
 // gRPC services setup
 let pkgDefinition = protoLoader.loadSync(
@@ -24,6 +26,7 @@ const client = new QuoteService(
 )
 
 
+// gRPC endpoints
 app.get('/api/grpc/quotes/', (req, res, next) => {
   client.get(null, (err, data) => {
     if (err) {
@@ -38,12 +41,37 @@ app.get('/api/grpc/quotes/', (req, res, next) => {
 app.get('/api/grpc/quotes/:id', (req, res, next) => {
   client.getOne({ id: parseInt(req.params.id) }, (err, data) => {
     if (err) {
-      res.send(err)
+      res.status(400).send(err)
       return next()
     }
 
     res.json(data)
   })
+})
+
+
+
+// REST endpoints
+app.get('/api/rest/quotes/', (req, res, next) => {
+  axios.get(`${SERVER_URL}/api/quotes/`)
+    .then((data) => {
+      res.json({ quotes: data.data });
+    })
+    .catch(err => {
+      res.status(400).send(err);
+      return next();
+    })
+})
+
+app.get('/api/rest/quotes/:id', (req, res, next) => {
+  axios.get(`${SERVER_URL}/api/quotes/${req.params.id}`)
+    .then((data) => {
+      res.json(data.data);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+      return next();
+    })
 })
 
 app.listen(REST_PORT, () => {
