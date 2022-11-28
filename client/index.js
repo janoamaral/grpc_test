6320ebb4f677c6ea8560a4b24a1636552bd306e9
Alejandro Amaral <aamaral@logico.com.ar>
2022-11-27 21:52:09 -0300

    Minimal setup working (#1)

--- client/index.js
+++ client/index.js
@@ -1,12 +1,84 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const REST_PORT = 3001
+const GRPC_PORT = 5000
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const axios = require('axios').default;
+const SERVER_URL = 'http://localhost:3000'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// gRPC services setup
+let pkgDefinition = protoLoader.loadSync(
+  "./src/grpc/protos/quotes.proto",
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    arrays: true,
+    defaults: true,
+    oneofs: true
+  })
+
+const QuoteService = grpc.loadPackageDefinition(pkgDefinition).QuoteService;
+const client = new QuoteService(
+  `127.0.0.1:${GRPC_PORT}`,
+  grpc.credentials.createInsecure()
+)
+
+
+// gRPC endpoints
+app.get('/api/grpc/quotes/', (req, res, next) => {
+  console.time("Get gRPC quotes")
+  client.get(null, (err, data) => {
+    if (err) {
+      res.status(err.code).send(err.details)
+      return next()
+    }
+
+    console.timeEnd("Get gRPC quotes")
+    return res.json(data)
+  })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/api/grpc/quotes/:id', (req, res, next) => {
+  client.getOne({ id: parseInt(req.params.id) }, (err, data) => {
+    if (err) {
+      res.status(400).send(err)
+      return next()
+    }
+
+    res.json(data)
+  })
+})
+
+
+
+// REST endpoints
+app.get('/api/rest/quotes/', (req, res, next) => {
+  console.time("Get REST quotes")
+  axios.get(`${SERVER_URL}/api/quotes/`)
+    .then((data) => {
+      console.timeEnd("Get REST quotes")
+      res.json({ quotes: data.data });
+    })
+    .catch(err => {
+      res.status(400).send(err);
+      return next();
+    })
+})
+
+app.get('/api/rest/quotes/:id', (req, res, next) => {
+  axios.get(`${SERVER_URL}/api/quotes/${req.params.id}`)
+    .then((data) => {
+      res.json(data.data);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+      return next();
+    })
+})
+
+app.listen(REST_PORT, () => {
+  console.log(`REST listening on ${REST_PORT}`)
 })
 
--- server/index.js
+++ server/index.js
@@ -1,12 +1,110 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const REST_PORT = 3000
+const GRPC_PORT = 5000
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const quotes = [
+  {
+    id: 1,
+    quote: "Frankly, my dear, I don't give a damn",
+    author: "Gone With the Wind"
+  },
+  {
+    id: 2,
+    quote: "I'm going to make him an offer he can't refuse",
+    author: "The Godfather"
+  },
+  {
+    id: 3,
+    quote: "Go ahead, make my day",
+    author: "Sudden Impact"
+  },
+  {
+    id: 4,
+    quote: "I love the smell of napalm in the morning",
+    author: "Apocalyse Now"
+  },
+  {
+    id: 5,
+    quote: "You're gonna need a bigger boat",
+    author: "Jaws"
+  },
+]
+
+function getQuoteById(id) {
+  // Return single object quote
+  return quotes.filter(quote => quote.id == id)[0] || {};
+}
+
+
+
+
+// Add REST routes
+app.get('/api/quotes/', (req, res) => {
+  res.json(quotes)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/api/quotes/:id', (req, res) => {
+  res.json(getQuoteById(req.params.id))
 })
 
+
+// gRPC services setup
+let pkgDefinition = protoLoader.loadSync(
+  "./src/grpc/protos/quotes.proto",
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    arrays: true,
+    defaults: true,
+    oneofs: true
+  })
+
+let quotesProto = grpc.loadPackageDefinition(pkgDefinition);
+
+const gRpcServer = new grpc.Server();
+
+// Add gRPC services
+gRpcServer.addService(quotesProto.QuoteService.service, {
+  get: (_, callback) => {
+    callback(null, { quotes })
+  },
+
+  getOne: (call, callback) => {
+    let quote = getQuoteById(call.request.id)
+
+    if (quote) {
+      callback(null, quote);
+    } else {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        details: `Quote id ${call.request.id} not found`
+      })
+    }
+  }
+});
+
+
+// Start both REST and gRPC servers
+async function serverStart() {
+  gRpcServer.bindAsync(
+    `127.0.0.1:${GRPC_PORT}`,
+    grpc.ServerCredentials.createInsecure(),
+    (err) => {
+      if (err) {
+        console.error(err.message);
+        return null;
+      }
+
+      gRpcServer.start();
+      console.log(`gRPC listening on ${GRPC_PORT}`)
+      app.listen(REST_PORT, () => {
+        console.log(`REST listening on ${REST_PORT}`)
+      })
+    });
+}
+
+serverStart()
